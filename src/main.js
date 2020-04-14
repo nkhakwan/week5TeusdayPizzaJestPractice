fe6d2693593e20c8d3cf84d6401722cbcb2f175f
nkhakwan <post_khan@yahoo.com>
2020-04-14 11:18:33 -0700

    more modifications made in various files

---
+++ src/main.js
@@ -0,0 +1,24 @@
+//import { PeanutButter } from './peanut-butter.js'
+//import '../css/styles.css'
+//import { Jelly } from './jelly.js'
+//import { Bread } from './bread.js'
+//import '../css/styles.css'
+import './styles.css';
+import { PizzaOrders } from './pizza';
+
+
+var pizzaOrder = new PizzaOrders();
+$(document).ready(function() {
+  displayOrderDetails(pizzaOrder.size, pizzaOrder.toppingOne, pizzaOrder.toppingTwo);
+  $("form").submit(function(event){
+    event.preventDefault();
+    var pizzaSize = parseInt($("#size").val());
+    var meatType  = parseInt($("#meat").val());
+    var vegType   = parseInt($("#veg").val());
+    pizzaOrder.addOrder(meatType,vegType,pizzaSize);
+   $("#cost").html(pizzaOrder.orderPrice[pizzaOrder.id]);
+   
+  });
+});
+
+
